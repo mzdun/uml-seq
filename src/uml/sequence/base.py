1f1554af80499c423089bc60916a5c14cf9c0875
Marcin Zdun <mzdun@midnightbits.com>
2013-03-03 21:59:00 +0100

    Adding an MIT-type license

--- src/uml/sequence/base.py
+++ src/uml/sequence/base.py
@@ -1,8 +1,33 @@
+# 
+# Copyright (C) 2013 midnightBITS/Marcin Zdun
+# 
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use, copy,
+# modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# 
+
 '''
 Created on 03-03-2013
 
-@author: Marcin
+@author: Marcin Zdun
 '''
+
 class Config:
     PAGE_MARGIN = 50
     OBJECT_DISTANCE = 125
@@ -13,19 +38,31 @@ class Config:
     STEP_OFFSET = 5
 
 class Diagram:
-    def object(self, name): return None
-    def pobject(self, name): return None
+    def object(self, name):
+        return None
+    def pobject(self, name):
+        return None
 
-    def async(self): pass
-    def sync(self): pass
-    def step(self): pass
+    def async(self):
+        pass
+    def sync(self):
+        pass
+    def step(self):
+        pass
 
-    def printOut(self, prn = None): pass
+    def printOut(self, prn = None):
+        pass
 
 class Object:
-    def active(self): pass
-    def inactive(self): pass
-    def sendTo(self, oth, name = None): return None
-    def returnTo(self, oth, name = None): return None
-    def create(self, oth): return None
-    def destroy(self, oth): return None
+    def active(self):
+        pass
+    def inactive(self):
+        pass
+    def sendTo(self, oth, name = None):
+        return None
+    def returnTo(self, oth, name = None):
+        return None
+    def create(self, oth):
+        return None
+    def destroy(self, oth):
+        return None
